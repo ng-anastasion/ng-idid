5d29141d8a4af251b4408887770ff81036a44a11
Anastasia Andreeva <frontaana@gmail.com>
2026-05-28 03:03:18 +0300

    added relative frequency configs

---
+++ libs/interview-tools/src/lib/resume-screener/configs/index.ts
@@ -0,0 +1,12 @@
+export * from './criteria.config';
+
+export * from './about-criteria.config';
+export * from './education-criteria.config';
+export * from './experience-criteria.config';
+export * from './header-criteria.config';
+export * from './job-criteria.config';
+export * from './skills-criteria.config';
+
+export * from './experience-keywords.config';
+export * from './market-skills.config';
+
